d03ffe81ac03bc86696e8304e6a8f469b892f5a9
wangguoxi <wangguoxi@darryring.com>
2025-12-22 22:10:09 +0800

    feat(security): 【权限控制】添加基于注解的权限控制功能，无法通过访问URL的方式访问资源
    - 在OrderController和ProductController中添加@Secured注解以限制访问角色
    - 配置spring-mvc.xml以启用Spring Security的注解支持
    - 添加security命名空间及schema位置以便使用安全标签
    - 启用全局方法安全性，支持secured-annotations、pre-post-annotations和jsr250-annotations

--- src/main/java/com/itheima/controller/OrderController.java
+++ src/main/java/com/itheima/controller/OrderController.java
@@ -1,11 +1,14 @@
 package com.itheima.controller;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/order")
 public class OrderController {
+
+    @Secured({"ROLE_ADMIN", "ROLE_ORDER"})
     @RequestMapping("/findAll")
     public String findAll(){
         return "order-list";
--- src/main/java/com/itheima/controller/ProductController.java
+++ src/main/java/com/itheima/controller/ProductController.java
@@ -1,11 +1,14 @@
 package com.itheima.controller;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/product")
 public class ProductController {
+
+    @Secured({"ROLE_ADMIN", "ROLE_PRODUCT"})
     @RequestMapping("/findAll")
     public String findAll(){
         return "product-list";
